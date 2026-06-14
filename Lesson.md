87d6e1084d67491741db63b926511b4d858bebb5
Kanto RAKOTONDRASOA <rakokanto007@gmail.com>
2026-06-14 16:55:16 +0300

    amelioration + question_15 + ToDoList

---
+++ Lesson.md
@@ -0,0 +1,11 @@
+# Methode pour voir le PID :
+
+```bash
+ps aux
+pgrep -a firefox
+top
+htop
+pidof sshd
+```
+
+# 
--- procwatch.sh
+++ procwatch.sh
@@ -16,7 +16,8 @@ echo "Choix :"
 read choix 
 
 if [ $choix != 1 ] && [ $choix != 2 ] && [ $choix != 3 ] && [ $choix != 4 ] && [ $choix != 5 ] && [ $choix != 6 ] && [ $choix != 7 ] && [ $choix != 8 ]; then
-    echo "Choix invalide"
+    echo "Choix invalide!!"
+    echo "Votre choix doit etre entre 0 a 9."
 
 elif [ $choix -eq 1 ]; then # 1
     ps aux --sort=-%cpu | head -n 11
@@ -25,30 +26,32 @@ elif [ $choix -eq 2 ]; then # 2
     ps aux --sort=-%mem | head -n 11
 
 elif [ $choix -eq 3 ]; then # 3
-    echo "Nom de processus: "
+    echo "Rechercher un processus par nom" 
+    echo "Inserer le NOM du processus : "
     read processus 
     if ! pgrep "$processus"; then
-        echo "processus $processus introuvable !"
+        echo "Processus $processus introuvable !"
     else
         pgrep -a $processus 
     fi
 
 elif [ $choix -eq 4 ]; then # 4
-    echo "Nom d'utilisateur: "
+    echo "Afficher les processus d'un utilisateur"
+    echo "Inserer le nom de l'utilisateur : "
     read utilisateur
     result=$(grep $utilisateur /etc/passwd)
-    echo $result
+    # echo $result
     if [ -z "$result" ]; then
-        echo "L'utilisateur $utilisateur n'existe pas"
+        echo "L'utilisateur $utilisateur n'existe pas."
     elif [ -n "$result" ]; then
         ps -u $utilisateur -f
     fi
 
 elif [ $choix -eq 5 ]; then # 5
-    echo "Votre PID: "
+    echo "Inserer le PID du processus : "
     read pid
     if [ -z "$pid" ]; then
-        echo "$pid inexistant"
+        echo "Processus $pid inexistant"
     elif [ ! -d "/proc/$pid" ]; then
         echo "$pid inexistant"
     elif [ "$pid" = "" ]; then
@@ -58,16 +61,16 @@ elif [ $choix -eq 5 ]; then # 5
     fi
 
 elif [ $choix -eq 6 ]; then # 6
-    echo "Votre PID: "
+    echo "Inserer le PID du processus : "
     read pid
     if [ -z "$pid" ]; then
-        echo "$pid inexistant"
+        echo "Processus $pid inexistant"
     elif [ ! -d "/proc/$pid" ]; then
-        echo "$pid inexistant"
+        echo "Processus $pid inexistant"
     elif [ "$pid" = "" ]; then
         echo "PID vide"
     elif [ $pid != "" ]; then
-        echo "Etes-vous sure $pid ? O/n"
+        echo "Etes-vous sure de forcer l'arret du processus $pid ? O/n"
         read reponse
         if [ $reponse != "O" ] && [ $reponse != "o" ]; then
             echo "Commande echouer"
@@ -77,12 +80,12 @@ elif [ $choix -eq 6 ]; then # 6
     fi
 
 elif [ $choix -eq 7 ]; then # 7
-    echo "Votre PID: "
+    echo "Inserer le PID du processus : "
     read pid
     if [ -z "$pid" ]; then
-        echo "$pid inexistant"
+        echo "Processus $pid inexistant"
     elif [ ! -d "/proc/$pid" ]; then
-        echo "$pid inexistant"
+        echo "Processus $pid inexistant"
     elif [ "$pid" = "" ]; then
         echo "PID vide"
     elif [ $pid != "" ]; then
